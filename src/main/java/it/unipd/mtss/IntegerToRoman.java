91aa9df92da4d0b689b649aee9790873a80bfa44
Alessio Silvestrini <200583814+Alessio-Silvestrini@users.noreply.github.com>
2026-05-04 16:45:11 +0200

    codice e test primi sei int
    aggiornato metodo, classi e test per i primi sei numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,10 +7,21 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
+
+        // PREcondizione
+        if (number < 1 || number > 6) { //funzionerebbe fino a 8, ma accetto i primi 6 come da consegna
+        throw new IllegalArgumentException("Sono ammessi solo numeri da 1 a 6.");
+    }
+
+        int[] values = {5, 4, 1}; // stringa con i valori base e sottrattivi in int
+        String[] symbols = {"V", "IV", "I"}; // stringa con i valori base e sottrattivi in roman
+
         StringBuilder convertedNumber = new StringBuilder();
-        while(number > 0) {
-            convertedNumber.append("I");
-            number -= 1;
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                convertedNumber.append(symbols[i]);
+                number -= values[i];
+            }
         }
 
         return convertedNumber.toString();
