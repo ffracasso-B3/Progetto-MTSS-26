d0543fb0c27c24a8cf2688ce0b7187c3e8d5d9da
Alessio-Silvestrini <200583814+Alessio-Silvestrini@users.noreply.github.com>
2026-05-05 15:29:29 +0200

    codice e test per cento
    aggiunto codice e test per i primi cento numeri

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -9,14 +9,14 @@ public class IntegerToRoman {
     public static String convert(int number) {
 
         // PREcondizione
-        if (number < 1 || number > 50) {
-        throw new IllegalArgumentException("Sono ammessi solo numeri da 1 a 50.");
+        if (number < 1 || number > 100) {
+        throw new IllegalArgumentException("Sono ammessi solo numeri da 1 a 100.");
     }
         // stringa con i valori base e sottrattivi in int
-        int[] values = {50, 40, 10, 9, 5, 4, 1}; 
+        int[] values = {100, 90, 50, 40, 10, 9, 5, 4, 1}; 
 
         // stringa con i valori base e sottrattivi in roman
-        String[] symbols = {"L", "XL", "X", "IX", "V", "IV", "I"}; 
+        String[] symbols = {"C", "XC" , "L", "XL", "X", "IX", "V", "IV", "I"}; 
 
         StringBuilder convertedNumber = new StringBuilder();
         
--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -13,17 +13,17 @@ public class RomanPrinter {
     private static String printAsciiArt(String romanNumber) {
         StringBuilder asciiPrint = new StringBuilder();
         
-        String numerals = "IVXL"; // supporta I, V, X e L
+        String numerals = "IVXLC"; // supporta I, V, X, L, C
 
         // String array bidimensionale per le asciiart
         String[][] asciiArts = {
 
-             { " _____ ", " _   _ ", "__   __" , " _     "}, // riga 1: I, V, X, L
-             { "|_   _|", "| | | |", "\\ \\ / /", "| |    " }, // riga 2
-             { "  | |  ", "| | | |", " \\ V / ", "| |    " }, // riga 3
-             { "  | |  ", "| | | |",  "  > <  ", "| |    "}, // riga 4
-             { " _| |_ ", " \\ V / ", " / . \\ ", "| |____ " }, // riga 5
-             { "|_____|", "  \\_/  ", "/_/ \\_\\", "|______|" }  // riga 6
+             { " _____ ", " _   _ ", "__   __", " _     ", "  _____"}, // riga 1: I, V, X, L, C
+             { "|_   _|", "| | | |", "\\ \\ / /", "| |    ", " / ____|"}, // riga 2
+             { "  | |  ", "| | | |", " \\ V / ", "| |    ", "| |" }, // riga 3
+             { "  | |  ", "| | | |",  "  > <  ", "| |    ", "| |"}, // riga 4
+             { " _| |_ ", " \\ V / ", " / . \\ ", "| |____ ", "| |____" }, // riga 5
+             { "|_____|", "  \\_/  ", "/_/ \\_\\", "|______|", " \\_____|" }  // riga 6
             
         };
 
