320a44b8cf67054f1f6dc5bfd8606daf17dfeae7
Alessio Silvestrini <200583814+Alessio-Silvestrini@users.noreply.github.com>
2026-05-05 19:36:39 +0200

    Fix "_" on D test

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -19,7 +19,7 @@ public class RomanPrinterTest {
         assertEquals("__   __\n\\ \\ / /\n \\ V / \n  > <  \n / . \\ \n/_/ \\_\\\n", RomanPrinter.print(10));
         assertEquals(" _      \n| |     \n| |     \n| |     \n| |____ \n|______|\n", RomanPrinter.print(50));
         assertEquals("  _____ \n / ____|\n| |     \n| |     \n| |____ \n \\_____|\n", RomanPrinter.print(100));
-        assertEquals(" _____  \n|  __ \\ \n| |  | |\n| |  | |\n| |__| |\n|_____/ \n", RomanPrinter.print(500));
+        assertEquals(" _____  \n|  __ \\ \n| |  | |\n| |  | |\n| |__| |\n|_____/\n", RomanPrinter.print(500));
         assertEquals(" __  __\n|  \\/  |\n| \\  / |\n| |\\/| |\n| |  | |\n|_|  |_|\n", RomanPrinter.print(1000));
     }
 }
